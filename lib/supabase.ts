e611935b12dec0ca3e65c55770573ec6ffb0193d
guiamorim1436 <gui.amorim1436@gmail.com>
2026-09-22 16:47:55 -0300

    Add lib/supabase.ts

---
+++ lib/supabase.ts
@@ -0,0 +1,11 @@
+﻿import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL || "https://ovnvidnfpvxsdtzbbazs.supabase.co";
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im92bnZpZG5mcHZ4c2R0emJiYXpzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc5MDA4OTMwNCwiZXhwIjoyMTA1NjY1MzA0fQ.tUsbW54dvVSrZ7Fxu_B2XBDsaBDErViX7L3NsOKkRGM";
+
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: false,
+    autoRefreshToken: false,
+  }
+});
